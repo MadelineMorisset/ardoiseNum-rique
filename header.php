50dfbeefee20f98dffb85d59bab05754165ecbcc
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-05-17 13:41:09 +0200

    Modification du lien de connexion

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     <nav>
         <a href="index.php" class="home"><i class="fa-solid fa-house"></i></i></a>
         <ul>
-            <li><a href="connexionAdmin.php" alt="Se connecter">Connexion / Admin</a></li>
-            <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+            <li><a href="login.php" alt="Se connecter">Connexion / Admin</a></li>
+            <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket" alt="Déconnexion"></i></a></li>
         </ul>
     </nav>
 </header>
