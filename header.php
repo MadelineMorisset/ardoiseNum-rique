f479c1ca19deeb28378b124ee7aa1929173fe147
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-05-17 13:32:54 +0200

    Ajout du header

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <a href="index.php" class="home"><i class="fa-solid fa-house"></i></i></a>
+        <ul>
+            <li><a href="connexionAdmin.php" alt="Se connecter">Connexion / Admin</a></li>
+            <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i></a></li>
+        </ul>
+    </nav>
+</header>
