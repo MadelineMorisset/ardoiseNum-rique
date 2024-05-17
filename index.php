3fecc3b579b35f6a945e2b1c797cf95aec078f26
Madeline Ricateau <madeline.ricateau@outlook.fr>
2024-05-17 12:00:31 +0200

    Ajout page d'accueil générale

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/23c1a897ea.js" crossorigin="anonymous"></script>
+    <!-- JavaScript -->
+    <script src="script.js"></script>
+    <title>Ardoise Numérique</title>
+</head>
+
+<body>
+    <?php include("header.php"); ?>
+
+    <main>
+        <h1>ARDOISE NUMÉRIQUE</h1>
+    </main>
+</body>
+
+</html>
